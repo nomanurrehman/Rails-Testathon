fec0be29ee34895583b3e2bdf55a374a87c4caac
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-03-12 17:08:50 +0100

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create user
+user = User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  date_of_birth: Faker::Date.birthday(min_age: 18, max_age: 50)
+)
+
+# Create products
+products = []
+0.upto(5) do
+  products << Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price: Faker::Commerce.price(range: 1.00..99999.99)
+  )      
+end
+
+# Create order
+order = user.orders.new(
+  due_date: Faker::Date.forward(days: 30)
+)
+order.products << products
+order.save
