b1264d7b724cffaf5a2b0d3c2838312a7b18b7b0
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-03-12 16:17:43 +0100

    Add create join table for orders and products table migration

---
+++ db/migrate/20200312141446_create_join_table_for_orders_products.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableForOrdersProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_join_table :orders, :products do |t|
+      # t.index [:order_id, :product_id]
+      # t.index [:product_id, :order_id]
+    end
+  end
+end
