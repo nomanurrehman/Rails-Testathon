de2ae15b8d43f106bce7ec2b79f9cc687471a17b
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-03-12 17:09:25 +0100

    Add product factory

---
+++ spec/factories/products.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :product do
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    price { Faker::Commerce.price(range: 1.00..99999.99) }
+  end
+end
