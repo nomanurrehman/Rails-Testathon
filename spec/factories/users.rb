3a8efd5c4ee9e8a6f60943f45dc4550f012db583
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-03-17 20:03:29 +0100

    Update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,16 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     date_of_birth { Faker::Date.birthday(min_age: 18, max_age: 50) }
+
+    # Generating users with orders
+    factory :user_with_orders do
+      transient do
+        orders_count { 10 }
+      end
+
+      after(:create) do |user, evaluator|
+        create_list(:order, evaluator.orders_count, user: user)
+      end
+    end
   end
 end
