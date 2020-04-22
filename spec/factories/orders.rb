c842dff2017d33a96e60161a509c3c9231390c6a
Noman Ur Rehman <nomanurrehman@gmail.com>
2020-04-22 17:36:28 +0200

    Update order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :order do
     due_date { Faker::Date.forward(days: 30)  }
-    association :user
-    # association :products, factory: :product
+    user
+    association :products, factory: :product
 
     # Generating orders with products
     # factory :order_with_products do
@@ -11,7 +11,18 @@ FactoryBot.define do
     #   end
 
     #   after(:create) do |order, evaluator|
-    #     create_list(:product, evaluator.products_count, orders: order)
+    #     create_list(:product, evaluator.products_count, orders: [order])
+    #   end
+    # end
+
+    # Generating orders with products
+    # factory :order_with_products do
+    #   transient do
+    #     products_count { 10 }
+    #   end
+
+    #   after(:create) do |order, evaluator|
+    #     create_list(:product, evaluator.products_count, orders: [order])
     #   end
     # end
   end
